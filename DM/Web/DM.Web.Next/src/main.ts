054863eeddb916cae49f34cc3a3c65cb8de9a68f
solohinlex <solohin85@yandex.ru>
2023-02-18 01:38:27 +0300

    базовая адаптивность

--- DM/Web/DM.Web.Next/src/main.ts
+++ DM/Web/DM.Web.Next/src/main.ts
@@ -4,11 +4,17 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDoorOpen, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faDoorOpen, faBars);
+
 import "@/assets/styles/refresh.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 
 app.mount("#app");
