e7fb87a7091d51dbe96142202a5bd5fd8b9b39de
solohinlex <solohin85@yandex.ru>
2023-02-18 03:20:46 +0300

    переключение тем с хранением в localStorage

--- DM/Web/DM.Web.Next/src/main.ts
+++ DM/Web/DM.Web.Next/src/main.ts
@@ -5,9 +5,13 @@ import App from "./App.vue";
 import router from "./router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faDoorOpen, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDoorOpen,
+  faBars,
+  faLightbulb,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-library.add(faDoorOpen, faBars);
+library.add(faDoorOpen, faBars, faLightbulb);
 
 import "@/assets/styles/refresh.css";
 
