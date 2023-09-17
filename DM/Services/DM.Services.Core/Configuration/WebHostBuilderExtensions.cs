dbde4a3e64b4f88bedb9bceac52347f878972596
Evengard <evengard@trioptimum.ru>
2023-09-17 23:28:35 +0300

    Rewriting configuration loading

--- DM/Services/DM.Services.Core/Configuration/WebHostBuilderExtensions.cs
+++ DM/Services/DM.Services.Core/Configuration/WebHostBuilderExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 
 namespace DM.Services.Core.Configuration;
 
@@ -14,5 +16,24 @@ public static class WebHostBuilderExtensions
     /// <param name="builder">Web host builder</param>
     /// <returns>Builder itself for chaining</returns>
     public static IHostBuilder WithDmConfiguration(this IHostBuilder builder) =>
-        builder.ConfigureAppConfiguration(configuration => configuration.AddEnvironmentVariables("DM_"));
+        builder.ConfigureAppConfiguration((ctx, cfg) =>
+        {
+            var env = ctx.HostingEnvironment;
+            var defaultNonJsonCfgs = cfg.Sources
+                .Where(s => s is not JsonConfigurationSource)
+                .ToArray();
+            cfg.Sources.Clear();
+            cfg
+                .AddJsonFile("secrets/appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("commonCfg/appsettings.json", optional: true, reloadOnChange: false)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .AddJsonFile($"secrets/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"commonCfg/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: false)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: false);
+            foreach (var defaultCfg in defaultNonJsonCfgs)
+            {
+                cfg.Add(defaultCfg);
+            }
+            cfg.AddEnvironmentVariables("DM_");
+        });
 }
